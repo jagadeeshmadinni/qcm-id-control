bb99130b9d724986be414b4a1ed22d04f7ce17bb
Jagadeesh Madinni <jmadinn@clemson.edu>
2024-02-10 15:25:50 -0500

    Add matlab script

---
+++ src/generateResponseQCM.m
@@ -0,0 +1,15 @@
+%Nominal values of the Quarter Car Suspension model from 
+% https://ctms.engin.umich.edu/CTMS/index.php?example=Suspension&section=SimulinkModeling
+% ignoring the damping effect of the tire
+%(m1) body mass 2500 kg
+%(m2) suspension mass 320 kg
+%(k1) spring constant of suspension system 80,000 N/m
+%(k2) spring constant of wheel and tire 500,000 N/m
+%(b1) damping constant of suspension system 350 N.s/m
+
+m1 = 2500;
+m2= 320;
+k1 = 80000;
+k2 = 500000;
+b1 = 350;
+
