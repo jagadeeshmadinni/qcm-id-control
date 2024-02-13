382c84145794c27abadce776b8751f8fb15c10d6
Jagadeesh Madinni <jmadinn@clemson.edu>
2024-02-13 16:48:12 -0500

    Updated parallelization parameter settings

--- src/generateResponseQCM.m
+++ src/generateResponseQCM.m
@@ -7,25 +7,23 @@
 %(k2) spring constant of wheel and tire 500,000 N/m
 %(b1) damping constant of suspension system 350 N.s/m
 
-s = rng(2);
-m1 = (1 + randi(40)/100)*2500;
-m2= (1 + randi(20)/100)*320;
-k1 = (1 + randi(40)/100)*80000;
-k2 = (1 + randi(40)/100)*500000;
-b1 = (1 + randi(20)/100)*350;
 
-parfor i = 1:100
+parfor i = 1
 
+    s = rng(i);
+    m1 = (1 + randi(40)/100)*2500;
+    m2= (1 + randi(20)/100)*320;
+    k1 = (1 + randi(40)/100)*80000;
+    k2 = (1 + randi(40)/100)*500000;
+    b1 = (1 + randi(20)/100)*350;
     in(i) = Simulink.SimulationInput('quarterCarModel');
     in(i) = setBlockParameter(in(i),'quarterCarModel/m1','mass',num2str(m1));
     in(i) = setBlockParameter(in(i),'quarterCarModel/k1','spr_rate',num2str(k1));
     in(i) = setBlockParameter(in(i),'quarterCarModel/b1','D',num2str(b1));
     in(i) = setBlockParameter(in(i),'quarterCarModel/m2','mass',num2str(m2));
     in(i) = setBlockParameter(in(i),'quarterCarModel/k2','spr_rate',num2str(k2));
-
     in(i) = setModelParameter(in(i),'StartTime','0','StopTime','60','FixedStep','0.1');
-    %sys = ss(A,B,C,D);
-    %fineZ(i,:)= step(sys,t);
+
 end 
 
 simOut = parsim(in, 'ShowSimulationManager', 'on');
