53af9c88783569049a595705ddcb1919aa6342c8
Jagadeesh Madinni <jmadinn@clemson.edu>
2024-02-14 16:53:50 -0500

    Model updates with new signal generator

--- src/generateResponseQCM.m
+++ src/generateResponseQCM.m
@@ -1,3 +1,5 @@
+clear vars;
+close all;
 %Nominal values of the Quarter Car Suspension model from 
 % https://ctms.engin.umich.edu/CTMS/index.php?example=Suspension&section=SimulinkModeling
 % ignoring the damping effect of the tire
@@ -7,8 +9,12 @@
 %(k2) spring constant of wheel and tire 500,000 N/m
 %(b1) damping constant of suspension system 350 N.s/m
 
+%According to NHTSA recommendation, speed bumps may not be more than 3
+%inches high. The input profile for the QCM model shall be a series of
+%bumps represented by a sine wave with an amplitude of 75 mm and separated by a
+%distance of 150 mm
 
-parfor i = 1
+parfor i = 1:10
 
     s = rng(i);
     m1 = (1 + randi(40)/100)*2500;
@@ -27,3 +33,21 @@ parfor i = 1
 end 
 
 simOut = parsim(in, 'ShowSimulationManager', 'on');
+
+for i = 1:10
+
+    x1_dot = squeeze(simOut(i).Road_vel.data);
+    x1 = squeeze(simOut(i).Road_y.data);
+    x2 = squeeze(simOut(i).susp_y.data);
+    x2_dot = squeeze(simOut(i).susp_vel.data);
+    xr_dot = squeeze(simOut(i).veh_vel.data);
+    xr = squeeze(simOut(i).veh_y.data);
+    t = simOut(i).tout;
+    plot(t,x1);
+    hold on;
+    plot(t,x2);
+    plot(t,xr);
+    legend("x1","x2","xr");
+    hold off;
+   
+end
